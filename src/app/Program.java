a6f7b59c2baf59cfbe3de555bab4f1ebc9a4f2ad
Sandro0330 <sandro030486@gmail.com>
2021-01-04 16:24:38 -0300

    Buscando id dos pedidos e produtos

--- src/app/Program.java
+++ src/app/Program.java
@@ -6,6 +6,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 import db.DB;
+import entities.Order;
+import entities.OrderStatus;
 import entities.Product;
 
 public class Program {
@@ -16,17 +18,32 @@ public class Program {
 	
 		Statement st = conn.createStatement();
 			
-		ResultSet rs = st.executeQuery("select * from tb_product");
+		ResultSet rs = st.executeQuery("SELECT * FROM tb_order " + 
+				"INNER JOIN tb_order_product ON tb_order.id = tb_order_product.order_id " + 
+				"INNER JOIN tb_product ON tb_product.id = tb_order_product.product_id ");
 			
 		while (rs.next()) {
 			
-			Product p = instanciandoProduto(rs);
+			Order order = instanciandoOrder(rs);
 			
-			System.out.println(p);
+			System.out.println(order);
 		}
 	}
 	
 	//Criando um método para instanciar um objeto
+	
+	private static Order instanciandoOrder(ResultSet rs) throws SQLException  {
+		Order order = new Order();
+		order.setId(rs.getLong("Id"));
+		order.setLatitude(rs.getDouble("latitude"));
+		order.setLongitude(rs.getDouble("longitude"));
+		order.setMoment(rs.getTimestamp("moment").toInstant());
+		order.setStatus(OrderStatus.values()[rs.getInt("status")]); //convertendo um inteiro para orderStatus
+		return order;
+	
+	}
+	
+	
 	private static Product instanciandoProduto(ResultSet rs) throws SQLException  {
 		Product p = new Product();
 		p.setId(rs.getLong("Id"));
